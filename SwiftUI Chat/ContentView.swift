67690b26eca57a0dff30907550d938d937d6aec5
Frank S. Bara <fbara1@yahoo.com>
2019-07-19 07:56:07 -0500

    Add List of messages.

--- SwiftUI Chat/ContentView.swift
+++ SwiftUI Chat/ContentView.swift
@@ -8,9 +8,33 @@
 
 import SwiftUI
 
+// structure that will represent each message in chat
+struct ChatMessage: Hashable {
+    var message: String
+    var avatar: String
+}
+
 struct ContentView: View {
+    
+    // add some dummy values to the messages
+    // suppose, there are only two messages in the chat room sent by two users: A and B
+    // A sent "Hello world" with a red message bubble color
+    // B sent "Hi" with a blue message color
+    var messages = [
+        ChatMessage(message: "Hello", avatar: "A"),
+        ChatMessage(message: "Hi", avatar: "B")
+    ]
+    
     var body: some View {
-        Text("Hello World")
+        List {
+            // we have several messages so we use the For Loop
+            ForEach(messages, id: \.self) {
+                // then we just show the avatars of the users and their messages
+                // by using these two Text functions
+                Text($0.avatar)
+                Text($0.message)
+            }
+        }
     }
 }
 
