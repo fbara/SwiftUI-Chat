85429f871a682afc07eeb84b4ab96e060c8a8096
Dennis <dennishasselbusch@icloud.com>
2019-10-18 21:39:50 +0200

    somechanges
    hey. Made some changes, so your project is run.
    #Hacktober

--- SwiftUI Chat/ContentView.swift
+++ SwiftUI Chat/ContentView.swift
@@ -7,9 +7,10 @@
 //
 
 import SwiftUI
+import Combine
 
-// structure that will represent each message in chat
-struct ChatMessage: Hashable {
+// let's create a structure that will represent each message in chat
+struct ChatMessage : Hashable {
     var message: String
     var avatar: String
     var color: Color
@@ -17,84 +18,183 @@ struct ChatMessage: Hashable {
     var isMe: Bool = false
 }
 
-// chatrow will be very similar to a cell in standard swift
-struct ChatRow: View {
-    // we will need to access and represent the chatMessages here
-    var chatMessage: ChatMessage
+struct ContentView : View {
+    // Necessary for moving the textField when keyboard appears
+    @ObservedObject private var kGuardian = KeyboardGuardian(textFieldCount: 1)
+    @State private var name = Array<String>.init(repeating: "", count: 1)
+    
+     // @State here is necessary to make the composedMessage variable accessible from different views
+    @State var composedMessage: String = ""
+    @EnvironmentObject var chatController: ChatController
     
     var body: some View {
-        Group {
-            //if the message is sent by the user, show it on the right side of the view
-            if !chatMessage.isMe {
-                HStack {
-                    Text(chatMessage.avatar)
-                    Text(chatMessage.message)
-                        .bold()
-                        .foregroundColor(Color.white)
-                        .padding(10)
-                        .background(chatMessage.color, cornerRadius: 10)
-                    Spacer()
+      
+        // the VStack is a vertical stack where we place all our substacks like the List and the TextField
+        VStack {
+            // I've removed the text line from here and replaced it with a list
+            // List is the way you should create any list in SwiftUI
+            List {
+                // we have several messages so we use the For Loop
+                ForEach(chatController.messages, id: \.self) { msg in
+                    ChatRow(chatMessage: msg)
                 }
-            } else {
-                // show the message on the left side of the view
-                HStack {
-                    Spacer()
-                    Text(chatMessage.message)
-                        .bold()
-                        .foregroundColor(Color.white)
-                        .padding(10)
-                        .background(chatMessage.color, cornerRadius: 10)
-                    Text(chatMessage.avatar)
+            }
+            
+            // TextField are aligned with the Send Button in the same line so we put them in HStack
+            HStack {
+                // this textField generates the value for the composedMessage @State var
+                TextField("Message...", text: $composedMessage).frame(minHeight: CGFloat(30))
+                    .foregroundColor(.black)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .background(GeometryGetter(rect : $kGuardian.rects[0]))
+                // the button triggers the sendMessage() function written in the end of current View
+                Button(action: sendMessage) {
+                    Text("Send")
                 }
+            }
+            .frame(minHeight: CGFloat(50)).padding()
+            .offset(y: kGuardian.slide)
+            .animation(.easeInOut(duration: 1.0))
+            // that's the height of the HStack
+        }.onAppear {
+            self.kGuardian.addObserver()
+        }.onDisappear{
+            self.kGuardian.removeObserver()
+        }
+        
+    }
+    func sendMessage() {
+        chatController.sendMessage(ChatMessage(message: composedMessage, avatar: "C", color: .green, isMe: true))
+        composedMessage = ""
+    }
+}
+// Following code is needed for moving textField.
+// You can find it here:
+/*https://stackoverflow.com/questions/56491881/move-textfield-up-when-thekeyboard-has-appeared-by-using-swiftui-ios */
+struct GeometryGetter: View {
+    @Binding var rect: CGRect
+
+    var body: some View {
+        GeometryReader { geometry in
+            Group { () -> AnyView in
+                DispatchQueue.main.async {
+                    self.rect = geometry.frame(in: .global)
+                }
+
+                return AnyView(Color.clear)
             }
         }
     }
 }
+final class KeyboardGuardian: ObservableObject {
+    public var rects: Array<CGRect>
+    public var keyboardRect: CGRect = CGRect()
 
-struct ContentView: View {
-    // @State here is necessary to make the composedMessage variable accessible from different views
-    @State var composedMessage: String = ""
-    @EnvironmentObject var chatController: ChatController
-    
-    // add some dummy values to the messages
-    // suppose, there are only two messages in the chat room sent by two users: A and B
-    // A sent "Hello world" with a red message bubble color
-    // B sent "Hi" with a blue message color
-    var messages = [
-        ChatMessage(message: "Hello", avatar: "A", color: .red),
-        ChatMessage(message: "Hi", avatar: "B", color: .blue)
-    ]
-    
-    var body: some View {
-        VStack {
-            List {
-                // we have several messages so we use the For Loop
-                ForEach(messages, id: \.self) {
-                    // then we just show the avatars of the users and their messages
-                    // by using these two Text functions
-                    ChatRow(chatMessage: $0)
-                }
-            }
-            
-            HStack {
-                TextField($composedMessage, placeholder: Text("Message..."))
-                    .frame(minHeight: 30)
-                Button(action: sendMesage) {
-                    Text("Send")
-                }
-            }.frame(height: 50)
-                .padding()
+    // keyboardWillShow notification may be posted repeatedly,
+    // this flag makes sure we only act once per keyboard appearance
+    public var keyboardIsHidden = true
+
+    @Published var slide: CGFloat = 0
+
+    var showField: Int = 0 {
+        didSet {
+            updateSlide()
         }
     }
-    
-    func sendMesage() {
-        chatController.sendMessage((ChatMessage(message: composedMessage, avatar: "C", color: .green, isMe: true)))
-        composedMessage = ""
+    init(textFieldCount: Int) {
+        self.rects = Array<CGRect>(repeating: CGRect(), count: textFieldCount)
+    }
+
+    func addObserver() {
+NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardDidHide(notification:)), name: UIResponder.keyboardDidHideNotification, object: nil)
+}
+
+func removeObserver() {
+ NotificationCenter.default.removeObserver(self)
+}
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+
+
+    @objc func keyBoardWillShow(notification: Notification) {
+        if keyboardIsHidden {
+            keyboardIsHidden = false
+            if let rect = notification.userInfo?["UIKeyboardFrameEndUserInfoKey"] as? CGRect {
+                keyboardRect = rect
+                updateSlide()
+            }
+        }
+    }
+
+    @objc func keyBoardDidHide(notification: Notification) {
+        keyboardIsHidden = true
+        updateSlide()
+    }
+
+    func updateSlide() {
+        if keyboardIsHidden {
+            slide = 0
+        } else {
+            let tfRect = self.rects[self.showField]
+            let diff = keyboardRect.minY - tfRect.maxY
+
+            if diff > 0 {
+                slide += diff
+            } else {
+                slide += min(diff, 0)
+            }
+
+        }
+    }
+}
+
+
+
+// ChatRow will be a view similar to a Cell in standard Swift
+struct ChatRow : View {
+    // we will need to access and represent the chatMessages here
+    var chatMessage: ChatMessage
+    // body - is the body of the view, just like the body of the first view we created when opened the project
+    var body: some View {
+        // HStack - is a horizontal stack. We let the SwiftUI know that we need to place
+        // all the following contents horizontally one after another
+        Group {
+            if !chatMessage.isMe {
+                HStack {
+                    Group {
+                        Text(chatMessage.avatar)
+                        Text(chatMessage.message)
+                            .bold()
+                            .padding(10)
+                            .foregroundColor(Color.white)
+                            .background(chatMessage.color)
+                            .cornerRadius(10)
+                    }
+                }
+            } else {
+                HStack {
+                    Group {
+                        Spacer()
+                        Text(chatMessage.message)
+                            .bold()
+                            .foregroundColor(Color.white)
+                            .padding(10)
+                            .background(chatMessage.color)
+                        .cornerRadius(10)
+                        Text(chatMessage.avatar)
+                    }
+                }
+            }
+        }
     }
 }
 
 #if DEBUG
-struct ContentView_Previews: PreviewProvider {
+struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
         ContentView()
         .environmentObject(ChatController())
